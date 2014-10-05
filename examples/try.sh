d1380c7bd552093c9da7228ae1a9f65e5900a67b
hex7c0 <carniellifrancesco@gmail.com>
2014-10-05 19:44:49 +0200

    update example

--- examples/try.sh
+++ examples/try.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 ../scaffold.py -sj try.json -d try git@github.com:hex7c0/nodejs-modules.git
+python3 ../scaffold.py -sj try2.json -d try git@github.com:hex7c0/nodejs-modules.git
